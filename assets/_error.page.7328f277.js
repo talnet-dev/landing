261977c2de941b81b5c054919aff154524abbd90
Andrey Antipov <586380+andreyantipov@users.noreply.github.com>
2022-04-18 09:51:22 +0400

    rebuild

---
+++ assets/_error.page.7328f277.js
@@ -0,0 +1,1 @@
+import{j as r,F as n,a as e}from"./renderer/_default.page.client.tsx.acdacf6e.js";function t({is404:o}){return o?r(n,{children:[e("h1",{children:"404 Page Not Found"}),e("p",{children:"This page could not be found."})]}):r(n,{children:[e("h1",{children:"500 Internal Server Error"}),e("p",{children:"Something went wrong."})]})}export{t as Page};
