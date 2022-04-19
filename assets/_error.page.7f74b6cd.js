3a429ed71c09a3e1a3798fc5d465905efcdf8aab
Andrey Antipov <586380+andreyantipov@users.noreply.github.com>
2022-04-19 15:30:11 +0400

    fix nested resources, resolver

--- assets/_error.page.7f74b6cd.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as n,a as e}from"./renderer/_default.page.client.tsx.7f22d12f.js";function t({is404:o}){return o?r(n,{children:[e("h1",{children:"404 Page Not Found"}),e("p",{children:"This page could not be found."})]}):r(n,{children:[e("h1",{children:"500 Internal Server Error"}),e("p",{children:"Something went wrong."})]})}export{t as Page};
